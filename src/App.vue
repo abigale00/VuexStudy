09de374a1fc7d90e33b2e9952d1fe7c41f8fa11d
zhangyuyao <17866627615@163.com>
2024-03-31 19:56:21 +0800

    Vuex study

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div>
+    hello world.
+    <hr />
+    <span>加法组件</span>
+    <addition-vue></addition-vue>
+    <span>减法组件</span>
+    <subtraction-vue></subtraction-vue>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import AdditionVue from "./components/Addition.vue";
+import SubtractionVue from "./components/Subtraction.vue";
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    SubtractionVue,
+    AdditionVue,
+  },
+};
+</script>
